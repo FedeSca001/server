dbabdd9ca32533e4baa758b9bb6bdc96771f475e
FedeSca001 <familadore@gmail.com>
2026-06-14 16:10:58 +0200

    db-sql

--- src/DataBase/db.js
+++ src/DataBase/db.js
@@ -1,17 +1,24 @@
 const mysql = require("mysql2");
 
 // Crear conexión a la base de datos
-const db = mysql.createConnection({
-    host: "localhost",
+const mysql = require("mysql2");
+
+const db = mysql.createPool({
+    host: "127.0.0.1",
     user: "scraper_user",
     password: "123456_asd",
-    database: "scraper"
+    database: "scraper",
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
+
 
 // Conectar
 db.connect((err) => {
     if (err) {
-        console.error("❌ Error conectando a la base de datos:", err.message);
+        console.error("❌ Error conectando a la base de datos:");
+        console.error(err); // <-- importante para ver el error real completo
         return;
     }
 
