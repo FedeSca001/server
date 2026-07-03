fc45ff6fad17d98964a5c0bfd98e819d58e1402a
FedeSca001 <familadore@gmail.com>
2026-07-03 17:08:27 +0200

    html

--- src/show-results/apiAlbums.js
+++ src/show-results/apiAlbums.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const db = require("../db/db.js");
+const db = require("../DataBase/db.js");
 
 const findAlbum = async (input) => {
     return new Promise((resolve, reject) => {
