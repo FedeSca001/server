9b4e22715e185123e3a53c1f53d1fda236b3cbfe
FedeSca001 <familadore@gmail.com>
2026-07-03 17:21:51 +0200

    html

--- src/html/index.js
+++ src/html/index.js
@@ -4,18 +4,14 @@ const btn = document.getElementById("buscarSql");
 
 async function buscarAlbums(texto) {
     try {
-        const response = await fetch(`/apiAlbums/${encodeURIComponent(texto)}`);
+        const response = await fetch(`http://192.168.1.148:3000/apiAlbums/${encodeURIComponent(texto)}`);
 
         if (!response.ok) {
             throw new Error("Error en la petición");
         }
-
         const albums = await response.json();
-
         console.log("Resultados encontrados:", albums);
-
         listado.innerHTML = "";
-
         for (const album of albums) {
             const li = document.createElement("li");
             li.textContent = `Título: ${album.title}, Imagen: ${album.image}, URL: ${album.url}`;
