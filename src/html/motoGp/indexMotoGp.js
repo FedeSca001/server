daebad26c904d43ab2943428eef8028970a4f953
FedeSca001 <familadore@gmail.com>
2026-07-21 18:04:57 +0200

    clasificacion-motogp

--- src/html/motoGp/indexMotoGp.js
+++ src/html/motoGp/indexMotoGp.js
@@ -4,33 +4,31 @@ const ClasificaciontList = document.getElementById("ClasificaciontList");
 const spinnerCarga = `<div class="spinner"></div>`;
 
 // Función para cargar la clasificación
-const renderCalendario = async () => {
-    // Mostrar spinner mientras carga
+const renderClasificacion = async () => {
     ClasificaciontList.innerHTML = spinnerCarga;
-
     try {
-        const response = await fetch("http://192.168.1.148:3000/apiMotoGp/clasificacion");
         //const response = await fetch("http://localhost:3000/apiMotoGp/clasificacion");
+        const response = await fetch("http://192.168.1.148:3000/apiMotoGp/clasificacion");
+
         if (!response.ok) {
             throw new Error(`Error ${response.status}: ${response.statusText}`);
         }
-
         const clasificacion = await response.json();
-        // Limpiar el spinner
         ClasificaciontList.innerHTML = "";
+        const puntosLider = clasificacion[0].puntos;
 
-        // Pintar la clasificación
         for (const piloto of clasificacion) {
+            const diferencia = puntosLider - piloto.puntos;
             const li = document.createElement("li");
-
             li.innerHTML = `
                 <p class="pilotoPosition">${piloto.posicion}</p>
                 <p class="pilotoNombre">${piloto.piloto}</p>
-                <p class="pilotoPuntos">${piloto.puntos} puntos</p>
+                <p class="pilotoPuntos">${piloto.puntos} pts</p>
+                <p class="pilotoDistancia">${diferencia === 0 ? "-" : "-" + diferencia}</p>
             `;
-
             ClasificaciontList.appendChild(li);
         }
+
     } catch (error) {
         console.error(error);
         ClasificaciontList.innerHTML = `<h2>${error.message}</h2>`;
@@ -38,4 +36,4 @@ const renderCalendario = async () => {
 };
 
 // Ejecutar al cargar la página
-renderCalendario();
+renderClasificacion();
